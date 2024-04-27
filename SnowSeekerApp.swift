d5f813ea3860891dca540f4ce681b80022c0c261
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-27 18:25:00 +0800

    Initial commit

---
+++ SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Liko Setiawan on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
